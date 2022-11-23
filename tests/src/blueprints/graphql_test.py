4a85370b5c285449cd3df8d1743f2d780c9995df
Caleb Reiter <ctr020196@gmail.com>
2022-11-23 00:02:24 -0600

    test(blueprint): add tests for graphql endpoint

---
+++ tests/src/blueprints/graphql_test.py
@@ -0,0 +1,29 @@
+import pytest
+
+
+def test_can_http_get_graphql(client):
+    response = client.get("/graphql")
+    assert response.json["error"]["message"] == "NOT FOUND"
+    assert response.status == "404 NOT FOUND"
+
+
+@pytest.mark.parametrize("client", [(True)], indirect=["client"])
+def test_can_http_get_graphql_debug_mode(client):
+    response = client.get("/graphql")
+    assert b"GraphQL Playground" in response.data
+    assert response.status == "200 OK"
+
+
+def test_can_http_post_graphql(client):
+    response = client.post(
+        "/graphql",
+        json={
+            "query": """query fetchAllTodos {
+                todos {
+                  success
+                }
+              }"""
+        },
+    )
+    assert response.json == {"data": {"todos": {"success": True}}}
+    assert response.status == "200 OK"
--- tests/src/conftest.py
+++ tests/src/conftest.py
@@ -1,14 +1,20 @@
 import pytest
+from _pytest.fixtures import SubRequest
 from flask.testing import FlaskClient
 
 from src.router import app
 
 
 @pytest.fixture()
-def client() -> FlaskClient:
+def client(request: SubRequest) -> FlaskClient:
     """Configure test client.
+
+    Args:
+        request (SubRequest): SubRequest for handling fixture parameters.
 
     Returns:
         FlaskClient: The Test Client
     """
+    debug = hasattr(request, "param") and request.param
+    app.debug = debug
     return app.test_client()
