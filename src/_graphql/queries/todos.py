045f2d745080926196d1f743294a2a8d5358ccd1
Caleb Reiter <ctr020196@gmail.com>
2022-11-26 16:37:58 -0600

    feat(graphql): use get_projection on resolver

--- src/_graphql/queries/todos.py
+++ src/_graphql/queries/todos.py
@@ -1,11 +1,11 @@
 """Queries for Todo schema."""
-
 from typing import Any, Dict
 
 from ariadne import convert_kwargs_to_snake_case
 from graphql.type import GraphQLResolveInfo
 
 from src._graphql import query
+from src.types.graphql import GraphQLUtils
 
 
 @query.field("todos")
@@ -35,5 +35,8 @@ def resolve_todo(_: Any, info: GraphQLResolveInfo, todo_id: int) -> Dict[str, An
     Returns:
         Dict[str, Any]: TodoResult
     """
+    utils = GraphQLUtils(info=info)
+    projection_fields = utils.get_projection("todo.todo")
     todo = {"id": 2, "completed": True, "description": "abc", "due_date": "2022-11-20"}
+    todo = {k: v for k, v in todo.items() if k in projection_fields}
     return {"success": True, "todo": todo}
